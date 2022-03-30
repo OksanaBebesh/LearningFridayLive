ed3b39ba38723bbd239198c81755b9f1abb01d95
OksanaBebesh <oksana.bebesh1984@gmail.com>
2022-03-30 17:24:24 +0300

    add tags click js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './scss/app.scss';
+
+window.onload = function() {
+    console.log("Hello OKsana")
+    //Tags
+    addTagsClickHandler();
+}
+
+const addTagsClickHandler = () => {
+document.querySelector(".strategies__tags").addEventListener('click', (e) => {
+   if (e.target.classList.contains('tag')){
+       let clickedTag = e.target;
+       removeSelectedTags();
+       selectClickedTag(clickedTag);
+   }
+})
+}
+
+const removeSelectedTags = () => {
+    let tags = document.querySelectorAll('.strategies__tags .tag');
+    tags.forEach((tag) => {
+        tag.classList.remove("tag_selected");
+        tag.classList.add('tag_bordered');
+    })
+}
+
+const selectClickedTag = (clickedTag) => {
+    clickedTag.classList.add('tag_selected');
+    clickedTag.classList.remove('tag_bordered');
+}
